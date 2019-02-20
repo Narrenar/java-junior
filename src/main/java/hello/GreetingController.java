64629a9c20971af6d7a192f4cbee84e82ee049bb
Narrenar <georgemoney007@mail.ru>
2019-02-20 21:48:40 +0300

    Add menu.html, news.html. Add improvements in previous files

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,9 +8,15 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class GreetingController {
 
+    @GetMapping("/menu")
+    public String menu(@RequestParam(name="menu", required=false, defaultValue="It's MAIN MENU, Body ;)") String menu, Model model) {
+        model.addAttribute("menu", menu);
+        return "menu";
+    }
+
     @GetMapping("/greeting")
-    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
+    public String greeting(@RequestParam(name="greeting", required=false, defaultValue="Body") String greeting, Model model) {
+        model.addAttribute("greeting", greeting);
         return "greeting";
     }
 
@@ -24,5 +30,11 @@ public class GreetingController {
     public String contacts(@RequestParam(name="contacts", required=false, defaultValue="+7(999)666-69-69, bestperson@inthe.uni") String contacts, Model model) {
         model.addAttribute("contacts", contacts);
         return "contacts";
+    }
+
+    @GetMapping("/news")
+    public String news(@RequestParam(name="news", required=false, defaultValue="Here must be some news but something went wrong") String news, Model model) {
+        model.addAttribute("news", news);
+        return "news";
     }
 }
